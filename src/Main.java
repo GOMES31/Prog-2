6b308b298f3e5124b51d69584c0ba64ab4c82f4a
Eduardo Gomes <dugomes02@gmail.com>
2022-10-12 12:19:44 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("M5","BMW",2018,52250.25);
-        System.out.println(vehicle.toString());
-        System.out.println("Price after 3 years: " + vehicle.getVehiclePrice(vehicle.getReleaseYear() + 2));
+
     }
 }
