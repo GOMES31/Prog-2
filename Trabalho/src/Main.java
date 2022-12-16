2f857e48c4f822af880bece75718eb92cc6200fe
Eduardo Gomes <95891238+GOMES31@users.noreply.github.com>
2022-12-16 18:58:07 +0000

    updated

--- Trabalho/src/Main.java
+++ Trabalho/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[]args){
-        Platform IT = new Platform();
+        Platform plat = new Platform();
+        plat.start();
     }
 }
--- Trabalho/src/Platform.java
+++ Trabalho/src/Platform.java
@@ -2,47 +2,101 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Platform {
-    public ArrayList<User> users;
+    private ArrayList<User> users;
+    private String[] startMenu,initialMenu;
+    private Scanner scan = new Scanner(System.in);
+    private String username,password;
 
     public Platform(){
         users = new ArrayList<>();
+        startMenu = new String[4];
+        initialMenu = new String[]
     }
 
-    public void registerUser(User user){
-        if(checkIfIsRegistered(user.getUsername())) System.out.println("Utilizador já registado!");
-        users.add(user);
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter("users.txt"));
-            writer.write("Username: " + user.getUsername() + "\tPassword: " + user.getPassword());
-            writer.close();
-        }catch(IOException e){
-            e.printStackTrace();
+    public void defineStartMenu(){
+        startMenu[0] = "----- MENU -----";
+        startMenu[1] = "1 - Iniciar Sessão";
+        startMenu[2] = "2 - Criar Conta";
+        startMenu[3] = "0 - [SAIR]";
+    }
+
+    public void defineInitialMenu(){
+        initialMenu[]
+    }
+
+    public void start(){
+        defineStartMenu();
+        System.out.println(String.join("\n", startMenu) + "\nEscolha uma opção!");
+        int option = -1;
+        while(option < 0 || option > 2){
+            option = scan.nextInt();
+            if(option < 0 || option > 2) System.out.println("Opção Inválida!\n" + String.join("\n", startMenu));
         }
-    }
-
-
-    public boolean checkIfIsRegistered(String username){
-        for(User u: users){
-            if(u.getUsername().equals(username)) return true;
-            break;
+        switch(option){
+            case 0:
+                System.out.println("Adeus!");
+                break;
+            case 1:
+                login();
+                break;
+            case 2:
+                register();
+                break;
         }
-        return false;
+        scan.close();
     }
 
-    public void startMenu(){
-        System.out.println("-- MENU --");
-        System.out.println("1 - Criar Conta");
-        System.out.println("2 - Iniciar Sessão");
-        System.out.println("0 - [SAIR]");
+    public void login(){
+        System.out.println("----- LOGIN MENU -----");
+        System.out.println("Insira o seu username!");
+        username = scan.next();
+        if(!checkIfUserIsRegistered(username)){
+            System.out.println("Utilizador não existe! \nTente criar uma conta ou então fazer login com outro username!");
+            start();
+        }
+        System.out.println("Insira a sua password!");
+        password = scan.next();
+        if(!checkIfPasswordIsCorrect(password)){
+            while(!checkIfPasswordIsCorrect(password)){
+                System.out.println("Password errada! Tente novamente");
+                password = scan.next();
+            }
+        }
+        initialMenu();
     }
 
-    public void mainMenu(){
-        System.out.println("-- MENU --");
-        System.out.println("1 - Skills");
-        System.out.println("2 - Talento");
-        System.out.println("2 - Propostas de Trabalho");
-        System.out.println("0 - [SAIR]");
+    public void register(){
+        System.out.println("----- REGISTER MENU -----");
+        System.out.println("Insira o username que pretende registar!");
+        username = scan.next();
+        if(checkIfUserIsRegistered(username)){
+            while(!checkIfUserIsRegistered(password)){
+                System.out.println("Username já registado! Tente novamente");
+                username = scan.next();
+            }
+        }
+        System.out.println("Insira a password que pretende usar!");
+        password = scan.next();
+        users.add(new User(username,password));
+        saveUserToCsvFile(new User(username,password));
+        initialMenu();
+    }
+
+    public void initalMenu(){
+        defineInitialMenu();
+    }
+    public boolean checkIfUserIsRegistered(String username){
+
+    }
+
+    public boolean checkIfPasswordIsCorrect(String password){
+
+    }
+
+    public void saveUserToCsvFile(User user){
+
     }
 }
